50116b3fd6b6968b2d61391f7d57d6a1a7f4fb14
Simon Friis Vindum <simonfv@gmail.com>
2021-03-04 07:46:31 +0100

    Add notation for language

---
+++ theories/lang/notation.v
@@ -0,0 +1,124 @@
+From iris.program_logic Require Import language.
+From self.lang Require Export lang.
+From iris.prelude Require Import options.
+
+(* The notation in this file matches HeapLang. We've only extended it with the
+additional operations related to weak and persistent memory. *)
+
+Coercion LitInt : Z >-> literal.
+Coercion LitBool : bool >-> literal.
+Coercion LitLoc : loc >-> literal.
+Coercion LitProphecy : proph_id >-> literal.
+
+Coercion App : expr >-> Funclass.
+
+Coercion Val : val >-> expr.
+Coercion Var : string >-> expr.
+
+Notation Lam x e := (Rec BAnon x e) (only parsing).
+Notation Let x e1 e2 := (App (Lam x e2) e1) (only parsing).
+Notation Seq e1 e2 := (Let BAnon e1 e2) (only parsing).
+Notation LamV x e := (RecV BAnon x e) (only parsing).
+Notation LetCtx x e2 := (AppRCtx (LamV x e2)) (only parsing).
+Notation SeqCtx e2 := (LetCtx BAnon e2) (only parsing).
+Notation Match e0 x1 e1 x2 e2 := (Case e0 (Lam x1 e1) (Lam x2 e2)) (only parsing).
+
+Notation Skip := (App (Val $ LamV BAnon (Val $ LitV LitUnit)) (Val $ LitV LitUnit)).
+
+Notation "# l" := (LitV l%Z%V%stdpp) (at level 8, format "# l").
+
+Notation "( e1 , e2 , .. , en )" := (Pair .. (Pair e1 e2) .. en) : expr_scope.
+Notation "( e1 , e2 , .. , en )" := (PairV .. (PairV e1 e2) .. en) : val_scope.
+
+Notation "'match:' e0 'with' 'InjL' x1 => e1 | 'InjR' x2 => e2 'end'" :=
+  (Match e0 x1%binder e1 x2%binder e2)
+  (e0, x1, e1, x2, e2 at level 200,
+   format "'[hv' 'match:'  e0  'with'  '/  ' '[' 'InjL'  x1  =>  '/  ' e1 ']'  '/' '[' |  'InjR'  x2  =>  '/  ' e2 ']'  '/' 'end' ']'") : expr_scope.
+Notation "'match:' e0 'with' 'InjR' x1 => e1 | 'InjL' x2 => e2 'end'" :=
+  (Match e0 x2%binder e2 x1%binder e1)
+  (e0, x1, e1, x2, e2 at level 200, only parsing) : expr_scope.
+
+Notation "()" := LitUnit : val_scope.
+Notation "- e" := (UnOp MinusUnOp e%E) : expr_scope.
+
+Notation "e1 + e2" := (BinOp PlusOp e1%E e2%E) : expr_scope.
+Notation "e1 +ₗ e2" := (BinOp OffsetOp e1%E e2%E) (at level 50, left associativity) : expr_scope.
+Notation "e1 - e2" := (BinOp MinusOp e1%E e2%E) : expr_scope.
+Notation "e1 * e2" := (BinOp MultOp e1%E e2%E) : expr_scope.
+Notation "e1 `quot` e2" := (BinOp QuotOp e1%E e2%E) : expr_scope.
+Notation "e1 `rem` e2" := (BinOp RemOp e1%E e2%E) : expr_scope.
+Notation "e1 ≪ e2" := (BinOp ShiftLOp e1%E e2%E) : expr_scope.
+Notation "e1 ≫ e2" := (BinOp ShiftROp e1%E e2%E) : expr_scope.
+
+Notation "e1 ≤ e2" := (BinOp LeOp e1%E e2%E) : expr_scope.
+Notation "e1 < e2" := (BinOp LtOp e1%E e2%E) : expr_scope.
+Notation "e1 = e2" := (BinOp EqOp e1%E e2%E) : expr_scope.
+Notation "e1 ≠ e2" := (UnOp NegOp (BinOp EqOp e1%E e2%E)) : expr_scope.
+
+Notation "~ e" := (UnOp NegOp e%E) (at level 75, right associativity) : expr_scope.
+
+(* Memory operations. *)
+Notation Alloc e := (AllocN (Val $ LitV $ LitInt 1) e) (only parsing).
+Notation "! e" := (Load e%E) (at level 9, right associativity) : expr_scope.
+Notation "!{acq} e" := (LoadAcquire e%E) (at level 9, right associativity) : expr_scope.
+Notation "'ref' e" := (Alloc e%E) (at level 10) : expr_scope.
+Notation "e1 <- e2" := (Store e1%E e2%E) (at level 80) : expr_scope.
+Notation "e1 <-{rel} e2" := (StoreRelease e1%E e2%E) (at level 80) : expr_scope.
+Notation CAS l e1 e2 := (Snd (CmpXchg l e1 e2)) (only parsing).
+
+Notation "'rec:' f x := e" := (Rec f%binder x%binder e%E)
+  (at level 200, f at level 1, x at level 1, e at level 200,
+   format "'[' 'rec:'  f  x  :=  '/  ' e ']'") : expr_scope.
+Notation "'rec:' f x := e" := (RecV f%binder x%binder e%E)
+  (at level 200, f at level 1, x at level 1, e at level 200,
+   format "'[' 'rec:'  f  x  :=  '/  ' e ']'") : val_scope.
+Notation "'if:' e1 'then' e2 'else' e3" := (If e1%E e2%E e3%E)
+  (at level 200, e1, e2, e3 at level 200) : expr_scope.
+
+Notation "'rec:' f x y .. z := e" := (Rec f%binder x%binder (Lam y%binder .. (Lam z%binder e%E) ..))
+  (at level 200, f, x, y, z at level 1, e at level 200,
+   format "'[' 'rec:'  f  x  y  ..  z  :=  '/  ' e ']'") : expr_scope.
+Notation "'rec:' f x y .. z := e" := (RecV f%binder x%binder (Lam y%binder .. (Lam z%binder e%E) ..))
+  (at level 200, f, x, y, z at level 1, e at level 200,
+   format "'[' 'rec:'  f  x  y  ..  z  :=  '/  ' e ']'") : val_scope.
+
+Notation "λ: x , e" := (Lam x%binder e%E)
+  (at level 200, x at level 1, e at level 200,
+   format "'[' 'λ:'  x ,  '/  ' e ']'") : expr_scope.
+Notation "λ: x y .. z , e" := (Lam x%binder (Lam y%binder .. (Lam z%binder e%E) ..))
+  (at level 200, x, y, z at level 1, e at level 200,
+   format "'[' 'λ:'  x  y  ..  z ,  '/  ' e ']'") : expr_scope.
+
+Notation "λ: x , e" := (LamV x%binder e%E)
+  (at level 200, x at level 1, e at level 200,
+   format "'[' 'λ:'  x ,  '/  ' e ']'") : val_scope.
+Notation "λ: x y .. z , e" := (LamV x%binder (Lam y%binder .. (Lam z%binder e%E) .. ))
+  (at level 200, x, y, z at level 1, e at level 200,
+   format "'[' 'λ:'  x  y  ..  z ,  '/  ' e ']'") : val_scope.
+
+Notation "'let:' x := e1 'in' e2" := (Lam x%binder e2%E e1%E)
+  (at level 200, x at level 1, e1, e2 at level 200,
+   format "'[' 'let:'  x  :=  '[' e1 ']'  'in'  '/' e2 ']'") : expr_scope.
+Notation "e1 ;; e2" := (Lam BAnon e2%E e1%E)
+  (at level 100, e2 at level 200,
+   format "'[' '[hv' '[' e1 ']' ;;  ']' '/' e2 ']'") : expr_scope.
+
+Notation "e1 && e2" :=
+  (If e1%E e2%E (LitV (LitBool false))) (only parsing) : expr_scope.
+Notation "e1 || e2" :=
+  (If e1%E (LitV (LitBool true)) e2%E) (only parsing) : expr_scope.
+
+Notation NONE := (InjL (LitV LitUnit)) (only parsing).
+Notation NONEV := (InjLV (LitV LitUnit)) (only parsing).
+Notation SOME x := (InjR x) (only parsing).
+Notation SOMEV x := (InjRV x) (only parsing).
+
+Notation "'match:' e0 'with' 'NONE' => e1 | 'SOME' x => e2 'end'" :=
+  (Match e0 BAnon e1 x%binder e2)
+  (e0, e1, x, e2 at level 200, only parsing) : expr_scope.
+Notation "'match:' e0 'with' 'SOME' x => e2 | 'NONE' => e1 'end'" :=
+  (Match e0 BAnon e1 x%binder e2)
+  (e0, e1, x, e2 at level 200, only parsing) : expr_scope.
+
+Notation ResolveProph e1 e2 := (Resolve Skip e1 e2) (only parsing).
+Notation "'resolve_proph:' p 'to:' v" := (ResolveProph p v) (at level 100) : expr_scope.
