10343c84096ea2cfe6013020e7973887b6ff87ea
Simon Friis Vindum <simonfv@gmail.com>
2021-08-28 11:48:07 +0200

    Prove more about crash borrow

--- src/base/crash_borrow.v
+++ src/base/crash_borrow.v
@@ -1,13 +1,17 @@
 From iris.algebra Require Import gmap auth agree gset coPset excl csum.
-From Perennial.program_logic Require Import staged_invariant.
+From Perennial.program_logic Require Import language staged_invariant.
 (* From Perennial.goose_lang Require Import crash_modality lifting wpr_lifting. *)
 (* From Perennial.goose_lang Require Import wpc_proofmode. *)
 From iris.proofmode Require Import tactics.
 From Perennial.base_logic.lib Require Import saved_prop frac_coPset.
 From Perennial.Helpers Require Import Qextra.
 
+(* From Perennial.program_logic Require Import language. *)
+From self.lang Require Import lang.
+From self.base Require Import post_crash_modality primitive_laws wpr_lifting.
 From self.base Require Import cred_frag.
 From self.base Require Import primitive_laws.
+From Perennial.program_logic Require Import language.
 
 Section frac_coPset_prop.
   Context {Σ : gFunctors}.
@@ -87,55 +91,49 @@ Section crash_borrow_def.
 
   Global Instance pri_invG_heap : pri_invG (nvmBaseG_irisGS).
   Proof.
-    Admitted.
-  (*   refine {| pri_inv_tok := λ q E, pinv_tok_inf q E |}; rewrite /pinv_tok_inf. *)
-  (*   - iIntros (??) "($&?)". *)
-  (*   - iIntros (???) "H". by rewrite ownfCP_inf_op_plus. *)
-  (*   - iIntros (???). rewrite ownfCP_inf_op_plus. by iIntros "$ $". *)
-  (*   - iIntros (q E) "H". by iApply (ownfCP_inf_le1). *)
-  (*   - iIntros (?????) "Hg". iDestruct "Hg" as "(?&?&?&?&?)". eauto. *)
-  (*   - iIntros (??????) "%Hlt Hg". iDestruct "Hg" as "(?&?&?&%Hle2&Hp)". *)
-  (*     iDestruct (ownfCP_op_plus with "Hp") as "(Hp1&$)". *)
-  (*     iFrame. iSplit. *)
-  (*     { iPureIntro. split; auto. transitivity (q1 + q2)%Qp; last by naive_solver. *)
-  (*       apply Qp_le_add_r. } *)
-  (*     iIntros (???) "Hg". iDestruct "Hg" as "(?&?&?&%Hle2'&Hp)". *)
-  (*     iFrame. iSplit; first auto. *)
-  (*     iApply ownfCP_op_plus. iFrame. *)
-  (*   - iIntros (?????) "%Hlt (Hg&Hp')". iDestruct "Hg" as "(?&?&?&%Hle2&Hp)". *)
-  (*     iFrame. iDestruct "Hp'" as "(%Hinf&Hp')". *)
-  (*     iDestruct (ownfCP_op_plus with "[$Hp' $Hp]") as "Hp". *)
-  (*     iDestruct (ownfCP_inf_le1 with "[$Hp //]") as %Hle3. *)
-  (*     iFrame. iPureIntro. *)
-  (*     split; auto. transitivity q2; first naive_solver. *)
-  (*     apply Qp_lt_add_r. *)
-  (*   - iIntros (g ns q D κ) "Hg". *)
-  (*     iMod (ownfCP_inf_init (coPset_name credit_cr_names)) as (E) "H". *)
-  (*     iDestruct "Hg" as "($&$&$&$&Hp)". *)
-  (*     iDestruct "H" as "(%Hinf&H)". *)
-  (*     iExists E. *)
-  (*     iDestruct (ownfCP_disj1 with "[$Hp H]") as %Hdisj. *)
-  (*     { iFrame; eauto. } *)
-  (*     iFrame. eauto. *)
-  (*   - iIntros (E g ns q D κ) "(Hp&Hg)". *)
-  (*     iDestruct "Hg" as "($&$&$&%Hle&Hp')". *)
-  (*     iFrame "%". *)
-  (*     iDestruct (ownfCP_disj_gt2 with "[$Hp $Hp']") as %Hdisj; first naive_solver. *)
-  (*     iDestruct "Hp" as "(Hinf&Hp)". *)
-  (*     iModIntro. iApply ownfCP_op_union; auto. *)
-  (*     iFrame. *)
-  (*   - iIntros (E g ns q D κ) "((%Hdisj&%Hinf)&Hg)". *)
-  (*     iDestruct "Hg" as "($&$&$&$&Hp)". *)
-  (*     iDestruct (ownfCP_op_union with "Hp") as "($&$)"; auto. *)
-  (*   - iIntros (g ns q1 q2 D κ E) "(Hg&Hp')". *)
-  (*     iDestruct "Hg" as "(?&?&?&%Hle&Hp)". *)
-  (*     iApply (ownfCP_disj with "[$]"). *)
-  (* Qed. *)
-
-  (* Section pre_borrow. *)
-
-  (* Context `{IRISG: !irisGS Λ Σ}. *)
-  (* Context `{LT: !later_tokG IRISG}. *)
+    refine {| pri_inv_tok := λ q E, pinv_tok_inf q E |}; rewrite /pinv_tok_inf.
+    - iIntros (??) "($&?)".
+    - iIntros (???) "H". by rewrite ownfCP_inf_op_plus.
+    - iIntros (???). rewrite ownfCP_inf_op_plus. by iIntros "$ $".
+    - iIntros (q E) "H". by iApply (ownfCP_inf_le1).
+    - intros (**). iIntros "(? & ? & ? & ?)". eauto.
+    - iIntros (??????) "%Hlt Hg". iDestruct "Hg" as "(?&?&%Hle2&Hp)".
+      iDestruct (ownfCP_op_plus with "Hp") as "(Hp1&$)".
+      iFrame. iSplit.
+      { iPureIntro. split; auto. transitivity (q1 + q2)%Qp; last by naive_solver.
+        apply Qp_le_add_r. }
+      iIntros (???) "Hg". iDestruct "Hg" as "(?&?&%Hle2'&Hp)".
+      iFrame. iSplit; first auto.
+      iApply ownfCP_op_plus. iFrame.
+    - iIntros (?????) "%Hlt (Hg&Hp')". iDestruct "Hg" as "(?&?&%Hle2&Hp)".
+      iFrame. iDestruct "Hp'" as "(%Hinf&Hp')".
+      iDestruct (ownfCP_op_plus with "[$Hp' $Hp]") as "Hp".
+      iDestruct (ownfCP_inf_le1 with "[$Hp //]") as %Hle3.
+      iFrame. iPureIntro.
+      split; auto. transitivity q2; first naive_solver.
+      apply Qp_lt_add_r.
+    - iIntros (g ns q D κ) "Hg".
+      iMod (ownfCP_inf_init (coPset_name credit_cr_names)) as (E) "H".
+      iDestruct "Hg" as "($&$&$&Hp)".
+      iDestruct "H" as "(%Hinf&H)".
+      iExists E.
+      iDestruct (ownfCP_disj1 with "[$Hp H]") as %Hdisj.
+      { iFrame; eauto. }
+      iFrame. eauto.
+    - iIntros (E g ns q D κ) "(Hp&Hg)".
+      iDestruct "Hg" as "($&$&%Hle&Hp')".
+      iFrame "%".
+      iDestruct (ownfCP_disj_gt2 with "[$Hp $Hp']") as %Hdisj; first naive_solver.
+      iDestruct "Hp" as "(Hinf&Hp)".
+      iModIntro. iApply ownfCP_op_union; auto.
+      iFrame.
+    - iIntros (E g ns q D κ) "((%Hdisj&%Hinf)&Hg)".
+      iDestruct "Hg" as "($&$&$&Hp)".
+      iDestruct (ownfCP_op_union with "Hp") as "($&$)"; auto.
+    - iIntros (g ns q1 q2 D κ E) "(Hg&Hp')".
+      iDestruct "Hg" as "(?&?&%Hle&Hp)".
+      iApply (ownfCP_disj with "[$]").
+  Qed.
 
   Definition pre_borrow : iProp Σ :=
     (later_tok ∗ later_tok ∗ later_tok ∗ later_tok).
@@ -266,9 +264,40 @@ Section crash_borrow_def.
     iApply (init_cancel_elim with "[$]"). eauto.
   Qed.
 
-  (*
+  Lemma wpc_borrow_inv s k E e Φ Φc :
+    (crash_borrow_ginv -∗ WPC e @ s; k; E {{ Φ }} {{ Φc }}) -∗
+    WPC e @ s; k; E {{ Φ }} {{ Φc }}.
+  Proof.
+    iIntros "H".
+    rewrite wpc_unfold.
+    iIntros (mj). rewrite /wpc_pre.
+    iSplit; last first.
+    { iIntros (????) "Hg HC".
+      iAssert (crash_borrow_ginv) with "[Hg]" as "#Hinv".
+      { iDestruct "Hg" as "(#Hinv&_)". eauto. }
+      iDestruct ("H" with "[$]") as "H".
+      iDestruct ("H" $! _) as "(_&H)".
+      iApply ("H" with "[$]"); eauto. }
+    destruct (to_val _).
+    - iIntros (?????) "Hg HNC".
+      iAssert (crash_borrow_ginv) with "[Hg]" as "#Hinv".
+      { iDestruct "Hg" as "(#Hinv&_)". eauto. }
+      iDestruct ("H" with "[$]") as "H".
+      iDestruct ("H" $! _) as "(H&_)".
+      iApply ("H" with "[$]"); eauto.
+    - iIntros (????????) "Hσ Hg HNC".
+      iAssert (crash_borrow_ginv) with "[Hg]" as "#Hinv".
+      { iDestruct "Hg" as "(#Hinv&_)". eauto. }
+      iDestruct ("H" with "[$]") as "H".
+      iDestruct ("H" $! _) as "(H&_)".
+      iApply ("H" with "[$] [$]"); eauto.
+  Qed.
+
+  (* Lemma wpc_crash_borrow_generate_pre s k (e : thread_state) Φ Φc : *)
+  (* language.to_val *)
+  (* language.language *)
   Lemma wpc_crash_borrow_generate_pre s k e Φ Φc :
-    language.to_val e = None →
+    to_val e = None →
     WPC e @ s; k; (⊤ ∖ (↑borrowN : coPset))
                     {{ v, pre_borrow -∗ Φ v }} {{ Φc }} -∗
     WPC e @ s; k; ⊤ {{ Φ }} {{ Φc }}.
@@ -320,6 +349,7 @@ Section crash_borrow_def.
     iApply "H". iFrame.
   Qed.
 
+  (*
   Lemma wp_crash_borrow_generate_pre s e Φ :
     language.to_val e = None →
     WP e @ s; (⊤ ∖ (↑borrowN : coPset))
