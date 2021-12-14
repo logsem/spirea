13a1ff1f288413d40cfa13e79a6879874125f541
Simon Friis Vindum <simonfv@gmail.com>
2021-12-14 13:16:07 +0100

    Add addition abstract state instances

--- src/high/abstract_state.v
+++ src/high/abstract_state.v
@@ -33,3 +33,45 @@ Proof. esplit; apply _. Defined.
 
 Lemma subseteq_nat_le (n m : nat) : n ⊑ m = (n ≤ m).
 Proof. done. Qed.
+
+(* Abstract state for unit. *)
+
+Instance sqsubseteq_unit : SqSubsetEq unit := λ u1 u2, True.
+
+Instance subseteq_unit_preorder : PreOrder (⊑@{unit}).
+Proof. done. Qed.
+
+Instance unit_abstract_state : AbstractState unit.
+Proof. esplit; apply _. Defined.
+
+(** * Discrete abstract state. *)
+
+Record discrete A := mk_discrete { get_discrete : A }.
+
+Arguments mk_discrete {_} _.
+Arguments get_discrete {_} _.
+
+Section discrete_abstract_state.
+  Context {A : Type}.
+
+  Global Program Instance discrete_eq_dec `{EqDecision A} : EqDecision (discrete A).
+  Next Obligation.
+    intros dec ??. unfold Decision.
+    decide equality. apply dec.
+  Qed.
+
+  Global Program Instance discrete_countable `{Countable A} : Countable (discrete A) :=
+    {| encode da := encode da.(get_discrete);
+       decode p := mk_discrete <$> decode p;
+    |}.
+  Next Obligation. intros ??[?]. rewrite decode_encode. done. Qed.
+
+  Global Instance discrete_sqsubseteq : SqSubsetEq (discrete A) := λ a1 a2, a1 = a2.
+
+  Global Instance subseteq_discrete_preorder : PreOrder (⊑@{discrete A}).
+  Proof. apply _. Qed.
+
+  Global Instance discrete_abstract_state `{Countable A} : AbstractState (discrete A).
+  Proof. esplit; apply _. Defined.
+
+End discrete_abstract_state.
