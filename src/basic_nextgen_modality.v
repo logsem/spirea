fdb6cbebda01b196fcdc42675ab487627d4645f9
Simon Friis Vindum <simonfv@gmail.com>
2023-07-07 19:21:18 +0200

    Weaken the requirements demanded by the basic nextgen modality

--- src/basic_nextgen_modality.v
+++ src/basic_nextgen_modality.v
@@ -4,6 +4,7 @@ From iris.base_logic.lib Require Export iprop own invariants.
 From iris.prelude Require Import options.
 
 From self Require Import extra cmra_morphism_extra.
+From self Require Export gen_trans.
 Import uPred.
 
 (** When working in the model, it is convenient to be able to treat [uPred] as
@@ -13,16 +14,19 @@ Local Coercion uPred_holds : uPred >-> Funclass.
 
 (* The _basic_ next-gen modality. *)
 Local Program Definition uPred_bnextgen_def {M : ucmra}
-  (f : M → M) `{!CmraMorphism f} (P : uPred M) : uPred M :=
-  {| uPred_holds n x := P n (f x) |}.
+  (t : M → M) `{!GenTrans t} (P : uPred M) : uPred M :=
+  {| uPred_holds n x := P n (t x) |}.
 Next Obligation.
-  naive_solver eauto using uPred_mono, cmra_morphism_monotoneN.
+  intros ????? n.
+  specialize (gen_trans_monoN t n) as monoN.
+  naive_solver eauto using uPred_mono, monoN.
 Qed.
 
 Local Definition uPred_bnextgen_aux : seal (@uPred_bnextgen_def).
 Proof. by eexists. Qed.
 
-Definition uPred_bnextgen {M : ucmra} f `{g : !CmraMorphism f} := uPred_bnextgen_aux.(unseal) M f g.
+Definition uPred_bnextgen {M : ucmra} f {g} :=
+  uPred_bnextgen_aux.(unseal) M f g.
 
 Local Definition uPred_bnextgen_unseal :
   @uPred_bnextgen = @uPred_bnextgen_def := uPred_bnextgen_aux.(seal_eq).
@@ -30,14 +34,16 @@ Local Definition uPred_bnextgen_unseal :
 Notation "⚡={ f }=> P" := (uPred_bnextgen f P)
   (at level 99, f at level 50, P at level 200, format "⚡={ f }=>  P") : bi_scope.
 
-Class IntoBnextgen `{M : ucmra} f `{!CmraMorphism f} (P : uPred M) (Q : uPred M) :=
+Class IntoBnextgen `{M : ucmra}
+    f `{!GenTrans f}
+    (P : uPred M) (Q : uPred M) :=
   into_bnextgen : P ⊢ ⚡={ f }=> Q.
 Global Arguments IntoBnextgen  {_} _%I {_} _%I _%I.
 Global Arguments into_bnextgen {_} _%I _%I {_}.
 Global Hint Mode IntoBnextgen + + + ! - : typeclass_instances.
 
 Section bnextgen_rules.
-  Context {M : ucmra} (f : M → M) `{!CmraMorphism f}.
+  Context {M : ucmra} (f : M → M) `{!GenTrans f}.
 
   Notation "P ⊢ Q" := (@uPred_entails M P%I Q%I) : stdpp_scope.
   Notation "⊢ Q" := (bi_entails (PROP:=uPredI M) True Q).
@@ -47,20 +53,18 @@ Section bnextgen_rules.
 
   Ltac unseal := try uPred.unseal; rewrite !uPred_bnextgen_unseal !/uPred_holds /=.
 
-  Global Instance bnextgen_ne : NonExpansive (uPred_bnextgen f).
-  Proof.
-    unseal. intros ? P Q Heq.
-    split.
-    intros ????. simpl.
-    split; intros ?; apply Heq; eauto using Heq, cmra_morphism_validN.
-  Qed.
-
   Lemma bnextgen_ownM (a : M) :
     uPred_ownM a ⊢ ⚡={f}=> uPred_ownM (f a).
   Proof.
-    unseal. split. simpl.
-    intros n x Hv ?.
-    apply cmra_morphism_monotoneN; done.
+    unseal. split. simpl. intros n x Hv ?. apply: gen_trans_monoN. done.
+  Qed.
+
+  #[global] Instance bnextgen_ne : NonExpansive (uPred_bnextgen f).
+  Proof.
+    unseal. intros ? P Q Heq.
+    split. intros ????. simpl.
+    apply Heq; first done.
+    apply: gen_trans_validN. done.
   Qed.
 
   Lemma bnextgen_and P Q :
@@ -71,35 +75,26 @@ Section bnextgen_rules.
     (⚡={f}=> P) ∨ (⚡={f}=> Q) ⊣⊢ ⚡={f}=> (P ∨ Q).
   Proof. unseal. split. simpl. done. Qed.
 
-  Lemma bnextgen_sep_2 P Q :
+  Notation "□ P" := (uPred_persistently P) : bi_scope.
+
+  (* Unlike [bnextgen_sep_2] this lemma does not depend on [CmraMorphism].
+   * Instead it relies on properties that are true for the core. TODO: Find the
+   * most general version of this lemma. *)
+  Lemma bnextgen_sep_2_alt P Q :
+    (∀ a, f a ≡ f a ⋅ f a) →
+    (∀ a b n, a ≼{n} b → f a ≼{n} f b) →
     (⚡={f}=> P) ∗ (⚡={f}=> Q) ⊢ ⚡={f}=> (P ∗ Q) .
   Proof.
-    unseal. split. simpl.
-    intros ? ? ?.
-    intros (a & b & eq & Hp & Hq).
-    exists (f a), (f b).
-    rewrite -(cmra_morphism_op _ a b).
-    rewrite eq. done.
-  Qed.
-
-  Lemma bnextgen_sep P Q :
-    (∀ n a b1 b2,
-       f a ≡{n}≡ b1 ⋅ b2 →
-       ∃ a1 a2, a ≡{n}≡ a1 ⋅ a2 ∧ f a1 ≡{n}≡ b1 ∧ f a2 ≡{n}≡ b2) →
-    (⚡={f}=> P) ∗ (⚡={f}=> Q) ⊣⊢ ⚡={f}=> (P ∗ Q) .
-  Proof.
-    intros cond.
-    apply (anti_symm _); first apply bnextgen_sep_2.
-    unseal. split. simpl.
-    intros ? a ?.
-    intros (b1 & b2 & eq & Hp & Hq).
-    destruct (cond n a b1 b2) as (a1 & a2 & ? & ? & ?); first done.
-    exists a1, a2.
-    subst.
-    split; first done.
-    rewrite H1.
-    rewrite H2.
-    split; done.
+    intros Hdup Hmono.
+    unseal.
+    split=> n x ?. intros (x1 & x2 & eq & ? & ?). simpl.
+    exists (f (x1 ⋅ x2)), (f (x1 ⋅ x2)). simpl in *.
+    split_and!.
+    - rewrite eq. rewrite -Hdup. done.
+    - eapply uPred_mono; first done; last done.
+      apply Hmono. apply cmra_includedN_l.
+    - eapply uPred_mono; first done; last done.
+      apply Hmono. apply cmra_includedN_r.
   Qed.
 
   Lemma bnextgen_intro_plainly P :
@@ -124,10 +119,10 @@ Section bnextgen_rules.
       eapply uPred_mono.
       - apply Hi.
         + eapply cmra_validN_le; last done.
-          apply: cmra_morphism_validN.
+          apply: gen_trans_validN.
           done.
         + done.
-      - apply: cmra_morphism_monotoneN. eexists _. done.
+      - apply: gen_trans_monoN. eexists _. done.
       - done.
     * intros Hi n' x' le val Hp.
       specialize (Hi n' ε le).
@@ -147,31 +142,12 @@ Section bnextgen_rules.
     unseal. split. simpl.
     intros ???.
     apply Hi.
-    apply: cmra_morphism_validN.
+    apply: gen_trans_validN.
     done.
   Qed.
 
   Lemma bnextgen_emp_2 : emp ⊢ ⚡={f}=> emp.
   Proof. unseal. done. Qed.
-
-  Lemma bnextgen_intuitionistically P :
-    (⚡={f}=> (<pers> P)) ⊣⊢ <pers> (⚡={f}=> P).
-  Proof.
-    unseal. split. simpl. intros ???.
-    pose proof (cmra_morphism_pcore f x) as eq.
-    rewrite 2!cmra_pcore_core in eq.
-    apply Some_equiv_inj in eq.
-    rewrite eq.
-    done.
-  Qed.
-
-  Lemma bnextgen_intuitionistically_1 P :
-    (⚡={f}=> (<pers> P)) ⊢ <pers> (⚡={f}=> P).
-  Proof. rewrite bnextgen_intuitionistically. done. Qed.
-
-  Lemma bnextgen_intuitionistically_2 P :
-    <pers> (⚡={f}=> P) ⊢ ⚡={f}=> (<pers> P).
-  Proof. rewrite bnextgen_intuitionistically. done. Qed.
 
   Global Instance bnextgen_mono' :
     Proper ((⊢) ==> (⊢)) (uPred_bnextgen f).
@@ -179,28 +155,6 @@ Section bnextgen_rules.
 
   Global Instance bnextgen_proper :
     Proper ((≡) ==> (≡)) (uPred_bnextgen f) := ne_proper _.
-
-  Lemma modality_bnextgen_mixin :
-    modality_mixin (@uPred_bnextgen M f _)
-      (MIEnvTransform (IntoBnextgen f)) (MIEnvTransform (IntoBnextgen f)).
-  Proof.
-    split; simpl; split_and?.
-    - intros ?? Hi.
-      rewrite Hi.
-      rewrite 2!intuitionistically_into_persistently.
-      apply bnextgen_intuitionistically_2.
-    - intros. rewrite bnextgen_and. done.
-    - done.
-    - apply bnextgen_emp_2.
-    - apply bnextgen_mono.
-    - apply bnextgen_sep_2.
-  Qed.
-  Definition modality_bnextgen :=
-    Modality _ modality_bnextgen_mixin.
-
-  Global Instance from_modal_bnextgen P :
-    FromModal True modality_bnextgen (⚡={f}=> P) (⚡={f}=> P) P | 1.
-  Proof. by rewrite /FromModal. Qed.
 
   Lemma bnextgen_later P :
     ▷ (⚡={f}=> P) ⊣⊢ ⚡={f}=> (▷ P).
@@ -249,7 +203,92 @@ Section bnextgen_rules.
 
 End bnextgen_rules.
 
-Lemma bnextgen_plain_soundness {M : ucmra} f `{!CmraMorphism f} (P : uPred M) `{!Plain P} :
+Section bnextgen_rules_cmra_morphism.
+  Context {M : ucmra} (f : M → M) `{!CmraMorphism f}.
+
+  Notation "P ⊢ Q" := (@uPred_entails M P%I Q%I) : stdpp_scope.
+  Notation "⊢ Q" := (bi_entails (PROP:=uPredI M) True Q).
+  Notation "(⊢)" := (@uPred_entails M) (only parsing) : stdpp_scope.
+
+  Local Arguments uPred_holds {_} !_ _ _ /.
+
+  Ltac unseal := try uPred.unseal; rewrite !uPred_bnextgen_unseal !/uPred_holds /=.
+
+  Lemma bnextgen_sep_2 P Q :
+    (⚡={f}=> P) ∗ (⚡={f}=> Q) ⊢ ⚡={f}=> (P ∗ Q) .
+  Proof.
+    unseal. split. simpl.
+    intros ???.
+    intros (a & b & eq & Hp & Hq).
+    exists (f a), (f b).
+    rewrite -(cmra_morphism_op _ a b).
+    rewrite eq. done.
+  Qed.
+
+  Lemma bnextgen_sep P Q :
+    (∀ n a b1 b2,
+       f a ≡{n}≡ b1 ⋅ b2 →
+       ∃ a1 a2, a ≡{n}≡ a1 ⋅ a2 ∧ f a1 ≡{n}≡ b1 ∧ f a2 ≡{n}≡ b2) →
+    (⚡={f}=> P) ∗ (⚡={f}=> Q) ⊣⊢ ⚡={f}=> (P ∗ Q) .
+  Proof.
+    intros cond.
+    apply (anti_symm _); first apply bnextgen_sep_2.
+    unseal. split. simpl.
+    intros ? a ?.
+    intros (b1 & b2 & eq & Hp & Hq).
+    destruct (cond n a b1 b2) as (a1 & a2 & ? & ? & ?); first done.
+    exists a1, a2.
+    subst.
+    split; first done.
+    rewrite H1.
+    rewrite H2.
+    split; done.
+  Qed.
+
+  Lemma bnextgen_intuitionistically P :
+    (⚡={f}=> (<pers> P)) ⊣⊢ <pers> (⚡={f}=> P).
+  Proof.
+    unseal. split. simpl. intros ???.
+    pose proof (cmra_morphism_pcore f x) as eq.
+    rewrite 2!cmra_pcore_core in eq.
+    apply Some_equiv_inj in eq.
+    rewrite eq.
+    done.
+  Qed.
+
+  Lemma bnextgen_intuitionistically_1 P :
+    (⚡={f}=> (<pers> P)) ⊢ <pers> (⚡={f}=> P).
+  Proof. rewrite bnextgen_intuitionistically. done. Qed.
+
+  Lemma bnextgen_intuitionistically_2 P :
+    <pers> (⚡={f}=> P) ⊢ ⚡={f}=> (<pers> P).
+  Proof. rewrite bnextgen_intuitionistically. done. Qed.
+
+  Lemma modality_bnextgen_mixin :
+    modality_mixin (@uPred_bnextgen M f _)
+      (MIEnvTransform (IntoBnextgen f)) (MIEnvTransform (IntoBnextgen f)).
+  Proof.
+    split; simpl; split_and?.
+    - intros ?? Hi.
+      rewrite Hi.
+      rewrite 2!intuitionistically_into_persistently.
+      apply bnextgen_intuitionistically_2.
+    - intros. rewrite bnextgen_and. done.
+    - done.
+    - apply bnextgen_emp_2.
+    - apply bnextgen_mono.
+    - apply bnextgen_sep_2.
+  Qed.
+  Definition modality_bnextgen :=
+    Modality _ modality_bnextgen_mixin.
+
+  Global Instance from_modal_bnextgen P :
+    FromModal True modality_bnextgen (⚡={f}=> P) (⚡={f}=> P) P | 1.
+  Proof. by rewrite /FromModal. Qed.
+
+End bnextgen_rules_cmra_morphism.
+
+Lemma bnextgen_plain_soundness {M : ucmra} f `{!GenTrans f} (P : uPred M) `{!Plain P} :
   (⊢ ⚡={f}=> P) → ⊢ P.
 Proof.
   eapply bi_emp_valid_mono. etrans; last exact: bnextgen_plainly.
